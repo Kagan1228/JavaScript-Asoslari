7f397faaa6d9badb9f03626852cb36f21b7b368c
Muhammad <examplegmail.com>
2022-02-06 13:58:14 +0500

    first commit

---
+++ Shartli operatorlar(if-else,switch-case)/app.js
@@ -0,0 +1,28 @@
+"use strict";
+//shartli operatorlar
+
+//if else
+
+const age = 25;
+//////////////////////////////////////////////////////
+if(age < 20)
+{
+    console.log('Kichkina');
+}
+else{
+    console.log('Standart yosh');
+}
+
+////////////////////////////////////////////////////////
+if(age < 20)
+{
+    console.log('Kichkina');
+}
+else if(age == 25){
+    console.log('yosh teng');
+}
+else{
+    console.log('xatolik');
+}
+///////////////////////////////////////////////////////
+//switch case
