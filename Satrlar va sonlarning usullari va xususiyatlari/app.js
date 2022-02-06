b4a2508e9e94bac8f64c5c4fbdfec0fcf3966e75
Muhammad <examplegmail.com>
2022-02-06 17:25:10 +0500

    first commit

---
+++ Satrlar va sonlarning usullari va xususiyatlari/app.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const text = "Hello World";
+// const arr = [1,2,3,4,5,6];
+
+// console.log(arr.length);
+// console.log(text.toLowerCase());matni mayda qiladi
+// console.log(text.toUpperCase());bu matni katta harf qiladi
+// console.log(text.indexOf('World'));indexni aniqlab beradi
+// console.log(text.slice(1,11));kesadi
+// console.log(text.substring(2))kesadi
+// console.log(text.substr(0,6))kesadi
+
+const num = 8.8;
+console.log(Math.round(num))//bu butun songa aylantiradi
+
+const borderWidth = "12px";
+console.log(parseInt(borderWidth))//bu soni namberga o'tkazadi
