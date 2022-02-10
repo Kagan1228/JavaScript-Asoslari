2185136cfb9be85412e156eeb673cca334fc72e9
Muhammad <examplegmail.com>
2022-02-10 21:16:53 +0500

    first commit

---
+++ Havola orqali yoki o'tkazish Spread opratorlasri/app.js
@@ -0,0 +1,81 @@
+"use strict";
+
+//Havola orqali yoki o'tkazish orqali o'tkazish,Spread operator
+
+// let a = 8,
+// b= a;
+
+// console.log(b+5);
+// console.log(a);
+
+
+
+// const mers = bmw;
+// mers.color = 'white';
+
+function copyObj(obj){
+    let objCopy = {}
+    for(let key in obj){
+        objCopy[key] = obj[key];
+    }
+    return objCopy;
+}
+
+const spark = {
+    color:'green',
+    isBuy:false,
+    c:{
+        a:100,
+        b:200,
+    }
+}
+
+const audi = copyObj(spark)
+spark.c.a = 300
+audi.color = "coral"
+
+console.log(spark);
+console.log(audi);
+
+
+//2-yoli
+
+const nexiaR2 = {
+    color:"red",
+    isBuy:true
+}
+
+const extra = {
+    shum:100
+}
+
+console.log(Object.assign(nexiaR2,extra));
+
+const tiko = Object.assign({},nexiaR2);
+tiko.isBuy= false
+console.log(tiko);
+console.log(nexiaR2);
+
+const arr = [1,2,3,4];
+const NewArr = arr.slice();
+NewArr[1] = 22;
+console.log(NewArr);
+console.log(arr);
+
+const cars = ['bmw','merc','nexiaR3'],
+plane = ['german','turkish',"O`zbek"],
+technology = [...cars,...plane];
+console.log(technology);
+
+function number(a, b, ...c){
+    console.log(a,b,c);
+}
+number(1,2,3,4,5,6)
+
+function func(a,b,c){
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+const numbers = [1,2,3];
+func(...numbers)
--- Massivlar va psevdo massivlar/app.js
+++ Massivlar va psevdo massivlar/app.js
@@ -1,6 +1,11 @@
 "use strict";
 
 const arr = [1,2,3,4,5,6];
+//boshidan qo'shadi
+// arr.unshift(0);
+//boshidan olib tashlaydi
+// arr.shift()
+console.log(arr);
 
 
 //---------------masivga qo'shish va olib tashlash-----------------
@@ -22,12 +27,29 @@ const arr = [1,2,3,4,5,6];
 // for(let key of arr){
 //     console.log(arr);
 // }
-arr.forEach((item,index,arr)=>{
-    console.log(item);
-})
+// arr.forEach((item,index,arr)=>{
+//     console.log(item);
+// })
 
-const colors = ['red','green','crimson'];
+// const colors = ['red','green','crimson'];
 
-colors.forEach((item,index,arr)=>{
-    console.log(item, index,arr);
-})
+// colors.forEach((item,index,arr)=>{
+//     console.log(item, index,arr);
+// });
+
+// ========================================================
+const question = prompt('','');
+//massiv qiladi
+const answers = question.split(', ');
+console.log(answers);
+//bu string fomatga otkazadi
+console.log(answers.join('; '))
+
+const number = [2,10,17,24,8];
+//faqatgina boshdagi qiymatdan znachenya qiladi 1dan boshlaydi
+number.sort(sorrt);
+console.log(number);
+
+function sorrt(a,b){
+    return a -b
+}
